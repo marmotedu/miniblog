b236caab353b0e13da550be79f9b7f327ea6e53b
belm(孔令飞) <colin404@foxmail.com>
2022-12-22 21:30:52 +0800

    feat: 开发以实现期望的 viper 配置读取功能

--- internal/miniblog/helper.go
+++ internal/miniblog/helper.go
@@ -8,12 +8,17 @@ package miniblog
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 const (
+	// recommendedHomeDir 定义放置 miniblog 服务配置的默认目录.
+	recommendedHomeDir = ".miniblog"
+
 	// defaultConfigName 指定了 miniblog 服务的默认配置文件名.
 	defaultConfigName = "miniblog.yaml"
 )
@@ -29,10 +34,13 @@ func initConfig() {
 		// 如果获取用户主目录失败，打印 `'Error: xxx` 错误，并退出程序（退出码为 1）
 		cobra.CheckErr(err)
 
-		// 将用 `$HOME` 目录加入到配置文件的搜索路径中
-		viper.AddConfigPath(home)
+		// 将用 `$HOME/<recommendedHomeDir>` 目录加入到配置文件的搜索路径中
+		viper.AddConfigPath(filepath.Join(home, recommendedHomeDir))
 
-		// 设置配置文件格式为 YAML(YAML格式清晰易读，并且支持复杂的配置结构)
+		// 把当前目录加入到配置文件的搜索路径中
+		viper.AddConfigPath(".")
+
+		// 设置配置文件格式为 YAML (YAML 格式清晰易读，并且支持复杂的配置结构)
 		viper.SetConfigType("yaml")
 
 		// 配置文件名称（没有文件扩展名）
@@ -42,8 +50,18 @@ func initConfig() {
 	// 读取匹配的环境变量
 	viper.AutomaticEnv()
 
+	// 读取环境变量的前缀为 MINIBLOG，如果是 miniblog，将自动转变为大写。
+	viper.SetEnvPrefix("MINIBLOG")
+
+	// 以下 2 行，将 viper.Get(key) key 字符串中 '.' 和 '-' 替换为 '_'
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+
 	// 读取配置文件。如果指定了配置文件名，则使用指定的配置文件，否则在注册的搜索路径中搜索
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 	}
+
+	// 打印 viper 当前使用的配置文件，方便 Debug.
+	fmt.Fprintln(os.Stdout, "Using config file:", viper.ConfigFileUsed())
 }
